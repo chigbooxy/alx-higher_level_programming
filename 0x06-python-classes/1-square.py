bad472622eeec4790a43da0bf87a02320469eb80
Triad01 <luckypee01@gmail.com>
2023-12-22 02:56:34 +0100

    all

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines the classs 'Square' with an ttribute"""
+
+
+class Square:
+    """
+        This is the square class
+        Attributes:
+            size: This is the size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
